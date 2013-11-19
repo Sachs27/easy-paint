911deb1a00bc81e99d4ee2e00f413cd0d26fb785
Ceouvre <ceouvre@gmail.com>
2013-11-19 18:12:02 +0800

    update

--- src/user_paint_panel.h
+++ src/user_paint_panel.h
@@ -10,6 +10,7 @@
 #include "record.h"
 #include "ui_toolbox.h"
 #include "ui_imagebox.h"
+#include "ui_color_picker.h"
 
 struct resource_manager;
 
@@ -17,36 +18,37 @@ struct resource_manager;
 struct user_paint_panel {
     struct ui           ui;             /* inherit from ui, so this must be
                                          * the first element of the struct  */
-    struct canvas       canvas;
-    struct record       record;
+    struct canvas           canvas;
+    struct record           record;
 
-    struct ui_toolbox   toolbox;
+    struct ui_color_picker  color_picker;
+    struct ui_toolbox       toolbox;
 
-    struct texture     *undo_image;
-    struct ui_imagebox  undo;
+    struct texture         *undo_image;
+    struct ui_imagebox      undo;
 
-    struct texture     *redo_image;
-    struct ui_imagebox  redo;
+    struct texture         *redo_image;
+    struct ui_imagebox      redo;
 
-    struct ui_imagebox  brush;
+    struct ui_imagebox      brush;
 
-    struct ui_toolbox   brushbox;
+    struct ui_toolbox       brushbox;
 
-    struct brush        brush_pen;
-    struct texture     *brush_pen_image;
-    struct ui_imagebox  brush_pen_icon;
+    struct brush            brush_pen;
+    struct texture         *brush_pen_image;
+    struct ui_imagebox      brush_pen_icon;
 
-    struct brush        brush_pencil;
-    struct texture     *brush_pencil_image;
-    struct ui_imagebox  brush_pencil_icon;
+    struct brush            brush_pencil;
+    struct texture         *brush_pencil_image;
+    struct ui_imagebox      brush_pencil_icon;
 
-    struct brush        brush_eraser;
-    struct texture     *brush_eraser_image;
-    struct ui_imagebox  brush_eraser_icon;
+    struct brush            brush_eraser;
+    struct texture         *brush_eraser_image;
+    struct ui_imagebox      brush_eraser_icon;
 
-    struct brush       *cur_brush;
+    struct brush           *cur_brush;
 
-    struct ui           blank;          /* blank ui for place holding */
+    struct ui               blank;          /* blank ui for place holding */
 };
 
 
